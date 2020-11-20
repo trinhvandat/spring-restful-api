5347f6a90862157791212eba3dc284e74d8bce28
trinhvandat <trinhvandat90399@gmail.com>
2020-11-20 23:58:58 +0700

    fix error edit

--- src/main/java/com/ptit/room/controller/RoomController.java
+++ src/main/java/com/ptit/room/controller/RoomController.java
@@ -3,16 +3,14 @@ package com.ptit.room.controller;
 import com.ptit.room.dto.RoomRequestDto;
 import com.ptit.room.dto.RoomResponseDto;
 import com.ptit.room.enumeration.RoomType;
+import com.ptit.room.model.Room;
 import com.ptit.room.service.RoomService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,16 +27,15 @@ public class RoomController {
     @ModelAttribute()
     public void addTypeRoomToModel(Model model){
         RoomType[] roomTypes = RoomType.values();
-        List<String> rooms = new ArrayList<>();
-        for (RoomType type : roomTypes){
-            rooms.add(type.toString());
-        }
-        model.addAttribute("rooms", rooms);
+
+        List<RoomResponseDto> rooms = roomService.getAllRooms();
+        model.addAttribute("roomLists", rooms);
+        model.addAttribute("roomType", roomTypes);
     }
 
     @GetMapping
     public String showIndexForm(){
-        return "index";
+        return "rooms";
     }
 
     @PostMapping("/rooms")
@@ -54,11 +51,32 @@ public class RoomController {
         return "redirect:/";
     }
 
+    @PostMapping("/rooms/update")
+    public String updateRoom(RoomRequestDto room){
+        logger.info("room: {}", room);
+        RoomResponseDto responseDto = roomService.updateRoom(room);
+        logger.info("response: {}", responseDto);
+        return "redirect:/";
+    }
+
     @GetMapping("/rooms")
     public String getListRoom(Model model){
         List<RoomResponseDto> rooms = roomService.getAllRooms();
         model.addAttribute("roomLists", rooms);
         return "rooms";
+    }
+
+    @GetMapping(value = "/rooms/{id}")
+    public String updateRoomForm(@PathVariable int id, Model model){
+        RoomResponseDto room = roomService.getRoomById(id);
+        model.addAttribute("room", room);
+        return "update";
+    }
+
+    @PostMapping("/rooms/{id}")
+    public String deleteRoom(@PathVariable int id){
+        roomService.deleteRoomById(id);
+        return "redirect:/";
     }
 
 }
